f4bd56335a76b571f05d5a690eb1b187529591c0
Zach McWhirter <zach@healthlock.com>
2021-07-22 14:38:26 -0500

    get checkbox working

--- src/App.js
+++ src/App.js
@@ -4,17 +4,32 @@ import './App.css';
 
 function App() {
 
-  const [buttonColor, setButtonColor ] = useState("red");
+  const [buttonColor, setButtonColor] = useState("red");
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
+
+  const [disabled, setDisabled] = useState(false);
+  // const isChecked = disabled === false ? true : false;
 
   return (
     <div>
       <button 
         style={{backgroundColor: buttonColor}} 
         onClick={() => setButtonColor(newButtonColor)}
-      >
-      Change to {newButtonColor}
+        disabled={disabled}
+      >Change to {newButtonColor}
       </button>
+      <input
+        // style={{}} 
+        type="checkbox"
+        defaultChecked={disabled}
+        // Allows the checkbox to default to whatever the default value is
+        // In this case the default is false
+        aria-checked={disabled}
+        // <aria-checked> allows screen readers to see the state of the checkbox
+        onChange={(e) => setDisabled(e.target.checked)}
+        // <checked> is a built-in that sets 
+        // or retrieves the boolean state of the check box
+      />
     </div>
   );
 }
